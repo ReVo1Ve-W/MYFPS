1609ed247569e4e14bd7f770a23c1eabc9710f67
叉烧 <ps488105674@outlook.com>
2026-06-02 23:19:10 +0800

    实现对人物的完全移动控制

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -7,24 +7,35 @@ public class PlayerControl : MonoBehaviour
 
     //速度
     public float moveSpeed = 5f;
+
+    public float jumpForce=5f;
     //灵敏度
     public float xSensitivity = 10;
+
     public float ySensitivity = 10;
 
     private float xRotation = 0;
-    private float yRotation = 0;
     private Rigidbody rb;
     private Animator anim;
+    private Vector3 velocity;
+    private bool jump=false;
+
     void Start()
     {
         rb = GetComponent<Rigidbody>();
         anim = GetComponentInChildren<Animator>();
+        Cursor.lockState=CursorLockMode.Locked;
     }
 
     // Update is called once per frame
     void Update()
     {
         Mouse();
+        HighSpeed();
+        Move();
+        Jump();
+        //transform.position+Vector3.up*0.2f,-Vector3.up,out hit,
+        //Debug.DrawRay(transform.position+Vector3.up*0.2f,-Vector3.up*0.4f,Color.red);
     }
     //鼠标旋转
     void Mouse()
@@ -40,8 +51,64 @@ public class PlayerControl : MonoBehaviour
         //左右旋转
         transform.Rotate(Vector3.up*xSensitivity*mouseX);
     }
+    void Move()
+    {
+        //获取输入
+        float horizontal = Input.GetAxis("Horizontal");  
+        float vertical = Input.GetAxis("Vertical");
+        //计算移动方向
+        Vector3 dir = transform.forward * vertical + transform.right * horizontal;
+        dir.Normalize();
+        //速度
+        velocity = dir * moveSpeed;
+        velocity.y = rb.velocity.y;
+        //动画
+        anim.SetFloat("Movement", dir.magnitude);
+       
+    }
+
+    void HighSpeed()
+    {
+        if(Input.GetKey(KeyCode.LeftShift)&&IsGround())
+        {
+            moveSpeed = 7;
+            anim.SetBool("Holstered",true);
+        }
+        else
+        {
+            moveSpeed = 5;
+             anim.SetBool("Holstered",false);
+
+        }
+    }
+
+    void Jump()
+    {
+        if (Input.GetKey(KeyCode.Space)&&IsGround())
+        {
+            jump=true;
+        }
+    }
+
+    public bool IsGround()
+    {
+        RaycastHit hit;
+        bool res = Physics.Raycast(transform.position+Vector3.up*0.2f,-Vector3.up,out hit,0.4f,LayerMask.GetMask("Ground"));
+        return res;
+    }
 
 
+
+
+    private void FixedUpdate()
+    {
+        if (jump)
+        {
+            jump=false;
+            velocity.y=jumpForce;
+        }
+        rb.velocity=velocity;
+    }
 
 
 
