1bb6dee003fae8dbc8f4216d919c825ebe45848b
叉烧 <ps488105674@outlook.com>
2026-06-03 17:05:38 +0800

    添加可破坏物

--- Assets/Scripts/BulletControl.cs
+++ Assets/Scripts/BulletControl.cs
@@ -20,6 +20,20 @@ public class BulletControl : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
+    //打到可破坏物
+        if (collision.gameObject.tag=="Des")
+        {
+            Rigidbody rigidbody= collision.gameObject.GetComponent<Rigidbody>();
+            if (rigidbody==null)
+            {
+                rigidbody= collision.gameObject.AddComponent<Rigidbody>();
+            }
+            rigidbody.AddForceAtPosition(transform.forward*5,collision.contacts[0].point,ForceMode.Impulse);
+            //Destroy(collision.gameObject.GetComponent<Collider>(),0.5f);
+            Destroy(collision.gameObject,2f);
+        }
+
+
         var effect= Instantiate(hitEffect,transform.position,Quaternion.LookRotation(collision.contacts[0].normal)); 
         Destroy(effect,1f);
         Destroy(gameObject);
